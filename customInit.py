b5d9704a3933ea94b4a881f5e12e0002ee2ad080
suhailnajeeb <suhailnajeeb19@gmail.com>
2018-11-22 14:07:56 +0600

    Custom Initializer: Bilinear Upsampling

---
+++ customInit.py
@@ -0,0 +1,20 @@
+from keras.initializers import Constant
+import numpy as np
+
+def upsample_filt(size):
+    factor = (size + 1) // 2
+    if size % 2 == 1:
+        center = factor - 1
+    else:
+        center = factor - 0.5
+    og = np.ogrid[:size, :size]
+    return (1 - abs(og[0] - center) / factor) * (1 - abs(og[1] - center) / factor)
+
+def bilinear_upsample_weights(factor, number_of_classes):
+    filter_size = factor*2 - factor%2
+    weights = np.zeros((filter_size, filter_size, number_of_classes, number_of_classes),
+                       dtype=np.float32)
+    upsample_kernel = upsample_filt(filter_size)
+    for i in range(number_of_classes):
+        weights[:, :, i, i] = upsample_kernel
+    return weights
